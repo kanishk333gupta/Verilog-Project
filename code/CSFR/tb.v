8330394cea291efe0deb22e47d5a4bcb82a7f403
kanishk333gupta <81621992+kanishk333gupta@users.noreply.github.com>
2021-03-30 17:08:33 +0530

    Add files via upload

---
+++ code/CSFR/tb.v
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+
+module CFSRtest;
+	// Inputs
+	reg clk;
+	reg rst;
+
+	// Outputs
+	wire [3:0] CFSR;
+
+	// Instantiate the Unit Under Test (UUT)
+	CFSR4bit uut (
+		.CFSR(CFSR), 
+		.clk(clk), 
+		.rst(rst)
+	);
+
+	initial
+		begin
+			 clk = 0; // setting intial clock 0 intially
+			 rst = 1; // setting reset 1 
+			 #15;     // delay of 15 unit of time
+			 rst = 0; // setting reset 0  
+			 #200;	 // delay of 200 unit of time
+		end
+
+	always
+		begin
+			 #5;
+			 clk = ~ clk; // changing clock every 5 ns
+		end
+		CFSR4bit DUT(CFSR,clk,rst); //DUT->Device Under Test
+endmodule
+
